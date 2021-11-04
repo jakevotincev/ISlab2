30dda260c56f972578c9e5f80534fcd11b5bd7d7
jakev <jakevotincev@gmail.com>
2021-11-05 01:22:08 +0300

    added decrypt method

--- src/Rijndael/RijndaelEncrypt.java
+++ src/Rijndael/RijndaelEncrypt.java
@@ -22,12 +22,37 @@ public class RijndaelEncrypt {
             0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
     };
 
+    private static final int[] INV_SBOX = {
+            0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+            0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+            0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+            0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+            0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+            0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+            0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+            0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+            0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+            0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+            0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+            0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+            0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+            0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+            0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+            0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+    };
+
     private static final int[][] CX = {
             {2, 3, 1, 1},
             {1, 2, 3, 1},
             {1, 1, 2, 3},
             {3, 1, 1, 2},
+    };
 
+    private static final int[][] INV_CX = {
+            {0xe, 0xb, 0xd, 0x9},
+            {0x9, 0xe, 0xb, 0xd},
+            {0xd, 0x9, 0xe, 0xb},
+            {0xb, 0xd, 0x9, 0xe}
     };
 
     private static final int[][] RCON = {
@@ -51,12 +76,34 @@ public class RijndaelEncrypt {
         for (int i = 1; i < 10; i++) {
             subBytes(state);
             shiftRows(state);
-            mixColumns(state);
+            mixColumns(state, false);
             addRoundKey(state, i);
         }
         subBytes(state);
         shiftRows(state);
         addRoundKey(state, 10);
+    }
+
+    public static void decrypt(int[][] state, int[][] cipherKey) {
+        keyExpansion(cipherKey);
+        addRoundKey(state, 10);
+        for (int i = 9; i > 0; i--) {
+            invShiftRows(state);
+            invSubBytes(state);
+            addRoundKey(state, i);
+            mixColumns(state, true);
+        }
+        invShiftRows(state);
+        invSubBytes(state);
+        addRoundKey(state, 0);
+    }
+
+    private static void invSubBytes(int[][] state) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                state[i][j] = INV_SBOX[state[i][j]];
+            }
+        }
     }
 
     private static void subBytes(int[][] state) {
@@ -73,6 +120,21 @@ public class RijndaelEncrypt {
         }
     }
 
+    private static void invShiftRows(int[][] state) {
+        for (int i = 1; i < 4; i++) {
+            rightRotate(state[i], i, 4);
+        }
+    }
+
+    private static void rightRotate(int[] arr, int d, int n) {
+        int[] temp = new int[n - d];
+        if (n - d >= 0) System.arraycopy(arr, 0, temp, 0, n - d);
+        for (int i = n - d; i < n; i++) {
+            arr[i - n + d] = arr[i];
+        }
+        if (n - d >= 0) System.arraycopy(temp, 0, arr, d, n - d);
+    }
+
     private static void shiftRows(int[][] state) {
         int[] newRow = new int[4];
         for (int i = 1; i < 4; i++) {
@@ -83,13 +145,16 @@ public class RijndaelEncrypt {
         }
     }
 
-    private static void mixColumns(int[][] state) {
+    private static void mixColumns(int[][] state, boolean inv) {
         for (int i = 0; i < 4; i++) {
             int[] column = new int[4];
             for (int j = 0; j < 4; j++) {
                 int r = 0;
                 for (int k = 0; k < 4; k++) {
-                    r ^= mult(state[k][i], CX[j][k]);
+                    if (inv)
+                        r ^= mult(state[k][i], INV_CX[j][k]);
+                    else
+                        r ^= mult(state[k][i], CX[j][k]);
                 }
                 column[j] = r;
             }
@@ -153,15 +218,40 @@ public class RijndaelEncrypt {
 
     private static int mult(int a, int c) {
         int result;
-        if (c < 3)
-            result = a * c;
-        else result = a * (c - 1) ^ a;
+        switch (c) {
+            case (0x2):
+                result = mul2(a);
+                break;
+            case (0x3):
+                result = mul2(a) ^ a;
+                break;
+            case (0x9):
+                result = mul2(mul2(mul2(a))) ^ a;
+                break;
+            case (0xb):
+                result = mul2(mul2(mul2(a))) ^ mul2(a) ^ a;
+                break;
+            case (0xd):
+                result = mul2(mul2(mul2(a))) ^ mul2(mul2(a)) ^ a;
+                break;
+            case (0xe):
+                result = mul2(mul2(mul2(a))) ^ mul2(mul2(a)) ^ mul2(a);
+                break;
+            default:
+                result = a * c;
+        }
         if (result > 255) {
-            result -= 0x100;
-            result = result ^ 0x1b;
+            result &= 0b11111111;
         }
         return result;
     }
 
-
+    private static int mul2(int a) {
+        if (a < 0x80) a *= 2;
+        else {
+            a = a * 2 ^ 0x1b;
+            a %= 0x100;
+        }
+        return a;
+    }
 }
